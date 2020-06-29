70027d8358f61232118534a6f148fd7a4ef0cc9e
Daniel Dao <fengyishou@gmail.com>
2020-06-29 19:14:51 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import First from './first';
+
+function App() {
+  return (
+    <div className="App">
+      <First />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+setInterval(First, 1000);
+export default App;
---
+++ src/first.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const element = React.createElement(
+    'h1',
+    {className: 'greeting'},
+    'Hello, world!'
+  );
+
+class Counter extends React.Component{
+    constructor(){
+        super();
+        this.state = {count: 0};
+
+        this.increment = this.increment.bind(this)
+    }
+    increment(e){
+        this.setState((state, v)=>({
+            count: state.count + 1
+        }));
+    }
+    render(){
+        return (
+            <div>
+                <input type="button" onClick={this.increment} value="hit me" />
+                <label>{this.state.count}</label>
+            </div>
+        );
+    }
+
+}
+
+class Clock extends React.Component{
+    constructor(){
+        super();
+        this.state = {date: new Date()};
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+          );
+    }
+    componentWillUnmount() {clearInterval(this.timerID);}
+    tick() {
+        this.setState({
+          date: new Date()
+        });
+      }
+    render() {
+        return (
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        );
+    }
+}
+
+function Frist() {    
+  return <div>{element}<Clock /><Counter /></div>
+}
+export default Frist;
